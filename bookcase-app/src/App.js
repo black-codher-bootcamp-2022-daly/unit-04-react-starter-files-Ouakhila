cbefebcc5643003deb72e362a4984e570aebcb41
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2022-12-02 11:27:41 +0000

    My first commit

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Book from './components/Book';
 import data from './models/books.json';
+import BookList from './components/BookList';
+
+function addBook(id) {
+  console.log(`The Book ${id} wa clicked`)
+}
 
 function App() {
   const books = data;
-  return <Book books={books}/>;
+  return (
+  // 
+  <BookList books={books} addBook={addBook}>
+  <Book books={books} onClick={() =>PaymentResponse.addBook(props.book.id)}/>
+  </BookList>
+  );
 }
 
 export default App;
---
+++ bookcase-app/src/components/BookList.js
@@ -0,0 +1,7 @@
+import Book from './components/Book';
+
+
+export function BookList () {
+    <Book book={Book}></Book>
+
+}
