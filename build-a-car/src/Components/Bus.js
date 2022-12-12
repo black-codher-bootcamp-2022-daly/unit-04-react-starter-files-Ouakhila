7bd67dc13eae1548235fc805bb6afb1981ceab1b
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2022-12-12 18:59:23 +0000

    React session 9

---
+++ build-a-car/src/Components/Bus.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Bus() {
+  return <h2>Hi, I am a Bus!</h2>;
+}
---
+++ build-a-car/src/Components/Stateboard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Stateboard() {
+  return <h2>Hi, I am a Stateboard!</h2>;
+}
